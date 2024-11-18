b3b80f0b1add1f8c330b863dcbe34967648194e4
subham2020btecs00002 <114304561+subham2020btecs00002@users.noreply.github.com>
2024-11-18 10:33:23 +0530

    Add files via upload
    Config Service is created for the ease to use application.properties from a common repository

---
+++ configserver-service/src/main/java/com/example/configserver/ConfigserverServiceApplication.java
@@ -0,0 +1,45 @@
+package com.example.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+@SpringBootApplication
+@RefreshScope
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigserverServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserverServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        return new RestTemplate();
+    }
+}
